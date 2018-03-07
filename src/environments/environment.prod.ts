dcb549485b846676a5854e7cd3d09d7051a62c7d
zeeeya <zci.alvarez@gmail.com>
2018-03-07 13:50:55 +0800

    Modified Environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,1 +1,13 @@
 
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyAb8OhkeYP2Y9WSTac6-_DaSLIQKLgH0Ls",
+        authDomain: "angular-social-65007.firebaseapp.com",
+        databaseURL: "https://angular-social-65007.firebaseio.com",
+        projectId: "angular-social-65007",
+        storageBucket: "angular-social-65007.appspot.com",
+        messagingSenderId: "728278989129"
+    
+    }
+};
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,12 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: 'APIKEY',
-        authDomain: 'DEV-APP.firebaseapp.com',
-        databaseURL: 'https://DEV-APP.firebaseio.com',
-        projectId: 'DEV-APP',
-        storageBucket: 'DEV-APP.appspot.com',
-        messagingSenderId: '123456789'
+        apiKey: "AIzaSyAb8OhkeYP2Y9WSTac6-_DaSLIQKLgH0Ls",
+        authDomain: "angular-social-65007.firebaseapp.com",
+        databaseURL: "https://angular-social-65007.firebaseio.com",
+        projectId: "angular-social-65007",
+        storageBucket: "angular-social-65007.appspot.com",
+        messagingSenderId: "728278989129"
+    
     }
 };
